fde888981fc57379ecdb86a8a11e2a82aa572208
EmirBigBoss <92429110+EmirBigBoss@users.noreply.github.com>
2023-05-13 12:55:43 +0200

    Try working with express now

--- TanksRequest.js
+++ TanksRequest.js
@@ -1,7 +1,30 @@
-const http = require('http');
-const server = http.createServer();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Wanna see tanks?');
+});
+
+app.get('/api/tanks', (req, res) => {
+    res.send(['T-34', 'M1-Abrams', 'Challanger 2']);
+})
+
+app.listen(3000, () => console.log('Listening on port 3000...'));
+
+
+/*const http = require('http');
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.write('Wanna see tanks?');
+        res.end();
+    }
+    if(req.url === '/api/tanks') {
+        res.write(JSON.stringify(['T-34', 'M1-Abrams', 'Challanger 2']));
+        res.end();
+    }
+});
 server.on('connection', (socket) => {
     console.log('New connection...');
 });
 server.listen(3000);
-console.log('Listening on port 3000...')
+console.log('Listening on port 3000...');*/
