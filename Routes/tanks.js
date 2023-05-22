311946a52b2a0332373c445a2d62c6dd5b678296
EmirBigBoss <92429110+EmirBigBoss@users.noreply.github.com>
2023-05-22 18:56:32 +0200

    Added ability to get tank by type, origin and name

--- Routes/tanks.js
+++ Routes/tanks.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const app = express();
 const router = express.Router();
+
 const tankExists = require('../Functions/tank_checker');
+app.use(express.urlencoded({extended: true}));
 const improveTankModel = require('../Functions/improve_tankmodel');
 const tankValidator = require('../Middleware functions/tank_validator')
 const Joi = require('joi');
@@ -15,7 +18,8 @@ const Tank = require('../Models/TankModel');
 
 
 
-router.get('/api/tanks', (req, res) => {
+router.get('/api/tanks', async (req, res) => {
+    const tanks = await Tank.find();
     res.send(tanks);
 });
 
@@ -36,14 +40,36 @@ router.post('/api/tanks', async (req, res) => {
         res.status(400).send(err.message);
     }
       
-})
+});
 
-router.get('/api/tanks/:id', (req, res) => {
-    const tank = tanks.find(t => t.id === parseInt(req.params.id));
 
-    if(!tank) return res.status(404).send('Tank with the given id doesn\'t exist.');
+router.get('/api/tanks/name/:name', async (req, res) => {
+
+    const tankName = req.params.name;
+    console.log(tankName);
+    const tank = await Tank.findOne({name: new RegExp(`${tankName}`, 'i')});
+
+    if(!tank) return res.status(404).send('Tank with the given name doesn\'t exist.');
     res.send(tank);
-})
+});
+
+router.get('/api/tanks/origin/:origin', async (req, res) => {
+
+    const tankOrigin = req.params.origin;
+    console.log(tankOrigin);
+    const tanks = await Tank.find({origin: new RegExp(`${tankOrigin}`, 'i')});
+    if(tanks.length === 0) return res.status(404).send('No tanks from given origin.');
+    res.send(tanks);
+});
+
+router.get('/api/tanks/type/:type', async (req, res) => {
+
+    const tankType = req.params.type;
+    console.log(tankType);
+    const tanks = await Tank.find({type: new RegExp(`${tankType}`, 'i')});
+    if(tanks.length === 0) return res.status(404).send('No tanks of given type.');
+    res.send(tanks);
+});
 
 router.put('/api/tanks/:id', (req, res) => {
     const tank = tanks.find(t => t.id === parseInt(req.params.id));
